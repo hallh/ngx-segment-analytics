bee6823155d4204bbfb86e48fbcd90d96f8e4f1d
TranDinhLap <trandinhlap87@gmail.com>
2020-09-07 16:09:13 +0700

    dist file

--- src/ngx-segment-analytics.config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Segment Configuration Interface
- */
-export interface SegmentConfig {
-    /** API Key for Segment.io */
-    apiKey: string;
-    /** Debug mode status */
-    debug?: boolean;
-}
